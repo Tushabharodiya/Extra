36ae8bb60d9dc1d5fe3d8b58af10ff9fb0adf28d
Tushabharodiya <147471467+Tushabharodiya@users.noreply.github.com>
2024-04-28 22:59:56 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+// let arr = [1,4, 3, 5, 2]
+// let arr2 = [7, 9, 8, 6, 10]
+// let arr3 = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//    arr3[i] = arr[i];
+// }
+// for (let i = 0; i < arr2.length; i++) {
+//    arr3[arr.length + i] = arr2[i]
+// }
+
+// for (let i = 0; i < arr.length + arr2.length; i++) {
+//    for (let j = 0; j < arr.length + arr2.length;j++) {
+//       if (arr3[i] < arr3[j]) {
+//          let temp = arr3[i]
+//          arr3[i] = arr3[j]
+//          arr3[j] = temp;
+//       }
+//    }
+// }
+// console.log(arr3);
+
+
